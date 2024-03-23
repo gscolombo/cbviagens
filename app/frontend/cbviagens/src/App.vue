4917ae5f0bd7b5e66c1e9735b1ed8f611a5266f2
Gabriel S. Colombo <gabriel.colombo@libertyti.com.br>
2024-03-23 17:45:35 -0300

    Início de implementação do frontend

---
+++ app/frontend/cbviagens/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import LeftPanel from './components/LeftPanel.vue';
+import CalculatorContainer from './components/calculator/CalculatorContainer.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <LeftPanel />
+    <main>
+      <CalculatorContainer />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    max-width: 100vw;
+    max-height: 100vh;
+    overflow-x: hidden;
+    display: grid;
+    grid-template-columns: max(min(25vw, 250px), 200px) 1fr;
+  }
+
+  main {
+    min-height: 100vh;
+    margin-top: 10vh;
+    background-color: whitesmoke;
+    box-shadow: inset 0px 4px 5px -2px rgba(0,0,0,0.5);
+  }
+</style>
